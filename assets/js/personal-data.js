b21e3ad25a76b8ed9d2ce4fc42f70189720fcdc3
Malte Brunnlieb <maybeec@users.noreply.github.com>
2017-07-16 17:05:55 +0200

    Update personal-data.js

--- assets/js/personal-data.js
+++ assets/js/personal-data.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     var personalViewPage = "personal-data.html";
     var currentUrl = window.location.href;
     if(currentUrl.endsWith(personalViewPage)) {
-        $("section:after").css("background", "#555");
-        $("section:after").css("color", "white");
-        $("section:after").css("background-image", "url('assets/images/portrait.jpg')");
-        $("section:after").css("background-position", "center right");
-        $("section:after").css("background", "no-repeat");
+        $("section").css("background", "#555");
+        $("section").css("color", "white");
+        $("section:after").css("content", "url('assets/images/portrait.jpg')");
+        //$("section:after").css("background-position", "center right");
+        //$("section:after").css("background", "no-repeat");
     }
 });
