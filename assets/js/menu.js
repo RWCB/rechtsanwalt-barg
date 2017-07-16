5478396641f0fea00210d2ebb76c7bc7dc7dca0b
Malte Brunnlieb <maybeec@users.noreply.github.com>
2017-07-16 14:59:56 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -7,6 +7,6 @@
     } else {
         var currentPage = currentUrl.substr(this.href.lastIndexOf('/') + 1);
         $("a[href$='" + currentPage + "']").addClass("active");
-        $("a:not([href$='" + currentPage + "'])").addClass("active");
+        $("a:not([href$='" + currentPage + "'])").removeClass("active");
     }
 })();
