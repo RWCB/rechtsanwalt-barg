93cdf0ae9e8783b3583624f4a9180cb17c917914
Malte Brunnlieb <maybeec@users.noreply.github.com>
2017-07-16 14:57:07 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,12 @@
 // highlight current page
 (function() {
-    var currentPage = $("body").data("current-page");
-    if (currentPage) {
-        $("a[href$='" + currentPage + ".html']").addClass("active");
+    var currentUrl = window.location.href;
+    if(currentUrl.endsWith('/')) {
+        $("a[href$='index.html']").addClass("active");
+        $("a:not([href$='index.html'])").removeClass("active");
+    } else {
+        var currentPage = currentUrl.substr(this.href.lastIndexOf('/') + 1);
+        $("a[href$='" + currentPage + "']").addClass("active");
+        $("a:not([href$='" + currentPage + "'])").addClass("active");
     }
 })();
