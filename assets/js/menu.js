79db80d2875bebc16c2cff1e32a6eccdccb04ffd
Malte Brunnlieb <maybeec@users.noreply.github.com>
2017-07-16 15:33:02 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,11 +3,9 @@
 (function() {
     var currentUrl = window.location.href;
     if(currentUrl.endsWith('/')) {
-        $("a").removeClass("activeLink");
         $("a[href$='index.html']").addClass("activeLink");
     } else {
         var currentPage = currentUrl.substr(currentUrl.lastIndexOf('/') + 1);
-        $("a").removeClass("activeLink");
         $("a[href$='" + currentPage + "']").addClass("activeLink");
     }
 })();
